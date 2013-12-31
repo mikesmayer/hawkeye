9ac63847c8830ea29f03140731cb6655b46c0bd3
Tyler Samuelson <tyler@Tylers-MacBook-Pro-2.local>
2013-12-31 12:10:27 -0600

    working on cap setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -26,7 +26,7 @@ server 'webfactional.com',
 	use_sudo: false, 
 	roles: %w{web app db}
 
-default_run_options[:pty] = true
+set :pty, true
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
