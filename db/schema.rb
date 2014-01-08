c8298c2bb577391d598d6530a87999788ba711fd
Tyler <tylersam@gmail.com>
2014-01-07 20:07:06 -0600

    ftp connect add

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140108013700) do
+ActiveRecord::Schema.define(version: 20140108020247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20140108013700) do
   create_table "approved_users", force: true do |t|
     t.string   "name"
     t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ftp_connects", force: true do |t|
+    t.string   "server"
+    t.string   "username"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
