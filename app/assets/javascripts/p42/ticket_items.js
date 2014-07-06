43a6adc3bf1ed24db2839efa9775b6f84b62ef46
Tyler <tylersam@gmail.com>
2014-07-06 15:14:41 -0700

    adding file list html template

--- app/assets/javascripts/p42/ticket_items.js
+++ app/assets/javascripts/p42/ticket_items.js
@@ -1,9 +1,14 @@
-if( $('#gdrive_file_list').length > 0 ){
-	$.ajax({
-		url: "file_list",
-		cache: false,
-		success: function(html){
-			$('#gdrive_file_list').html(html);
-		}
-	});
-}
+$(document).ready(function(){
+	if( $('#gdrive_file_list').length > 0 ){
+		console.log("Getting files from google drive");
+		$.ajax({
+			url: "file_list",
+			cache: false,
+			success: function(html){
+				console.log(html);
+				$('#gdrive_file_list').html(html);
+			}
+		});
+	}
+});
+
