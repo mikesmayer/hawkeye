759c0b5d63c5182ad24bf8cdf39257fa7654280e
Tyler <tylersam@gmail.com>
2014-08-12 16:56:00 -0700

    debug

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 RailsBootstrap::Application.initialize!
 
-ENV['RAILS_ENV'] = 'development' 
+#ENV['RAILS_ENV'] = 'development' 
